0aa8f4811f2e40ddee502a6e87c2f1f75b4a931c
liu <13125179586@163.com>
2018-01-29 11:23:26 +0800

    database commit

--- mydj/settings.py
+++ mydj/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'mydj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mydj',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'mydj',
+        'PASSWORD': '123456',
+        'HOST': '192.168.35.131',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',   
     }
 }
 
